628e1fc53fdf127942c8d945ad9f9f6d1f47d69a
Apple <apple@Apples-MacBook-Air.local>
2026-08-14 23:40:59 +0530

    install type script

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+var username = "aryaman";
+var age = 22;
+console.log(username);
+console.log(age);
---
+++ app.ts
@@ -0,0 +1,4 @@
+  var username:string="aryaman";
+  var age:number=22;
+  console.log(username);
+  console.log(age);
